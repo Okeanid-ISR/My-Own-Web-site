313d870dcb8d2593a4e0959e8fded02e8fc5807d
Okeanid-ISR <jewishsly@gmail.com>
2023-03-20 13:27:01 +0200

    Feat: collaboration component added

---
+++ src/components/Collaboration.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import "../styles/portfolio.css"
+import "../styles/collaboration.css"
+
+const Collaboration = () => {
+    return (
+        <div className="collaboration-main">
+            <div className="container">
+                <div className="collaboration-info text-center">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4 className="collaboration-main__collab">Interested in collaborations with me?</h4>
+                            <p className="fs-5 collaboration-info-desires"> I'd be happy to collaborate with you! Let's create something great together!</p>
+                        </div>
+                        <div className="col-12">
+                            <button className="btn-email">Email me</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Collaboration;
