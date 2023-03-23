6b4a09921ca8b3dfcb00b02d4959cdc6f5cac475
Okeanid-ISR <jewishsly@gmail.com>
2023-03-23 02:12:55 +0200

    Feat: responsive hobbies block added

--- src/components/Hobbies.jsx
+++ src/components/Hobbies.jsx
@@ -1,51 +1,26 @@
 import React from "react"
 import "../styles/hobbies.css"
+//toDo:создать компонент хобби карт
 
-const Hobbies = () => {
+const Hobbies = ({ar}) => {
     return (
         <div className="hobbies-main">
             <div className="container">
                 <div className="row">
-                    <div className="col-3">
-                        <div
-                            className="hobbies-blocks cursor-pointer d-flex align-items-center flex-column justify-content-evenly">
-                            <div className="hb-1"></div>
-                            <div className="text-center">
-                                <h4 className="hobbies-blocks__topic">Build Muscle</h4>
-                                <p>Guide</p>
+                    {ar.map((card) => {
+                        return (
+                            <div className="col-12 col-sm-6 col-md-6 col-xl-3 d-md-flex mb-5 hobbies_general col-md-3">
+                                <div
+                                    className="hobbies-blocks cursor-pointer d-flex align-items-center flex-column justify-content-evenly">
+                                    <div className={card.image}/>
+                                    <div className="text-center">
+                                        <h4 className="hobbies-blocks__topic">{card.title}</h4>
+                                        <p className="hobbies-blocks__guide">{card.definition}</p>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div
-                            className="hobbies-blocks cursor-pointer d-flex align-items-center flex-column justify-content-evenly">
-                            <div className="hb-3"></div>
-                            <div className="text-center">
-                                <h4 className="hobbies-blocks__topic">Tattoo Designs</h4>
-                                <p>Guide</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div
-                            className="hobbies-blocks cursor-pointer d-flex align-items-center flex-column justify-content-evenly">
-                            <div className="hb-4"></div>
-                            <div className="text-center">
-                                <h4 className="hobbies-blocks__topic">Food Receipts</h4>
-                                <p>Guide</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-3">
-                        <div
-                            className="hobbies-blocks cursor-pointer d-flex align-items-center flex-column justify-content-evenly">
-                            <div className="hb-2"></div>
-                            <div className="text-center">
-                                <h4 className="hobbies-blocks__topic">Education</h4>
-                                <p>Guide</p>
-                            </div>
-                        </div>
-                    </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
