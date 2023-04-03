a665e27781b51501750dc50effcb9bdc4b31a5b3
Okeanid-ISR <jewishsly@gmail.com>
2023-04-03 10:14:49 +0300

    Feat: tattoo json added

---
+++ src/components/comps_tattoo/tattoosArr.js
@@ -0,0 +1,62 @@
+export const tattoosArr = [
+    {
+        img: ["https://cdntattoofilter.com/tattoo/385558/l.jpg", "https://cdntattoofilter.com/tattoo/383301/l.jpg", "https://cdntattoofilter.com/tattoo/382625/l.jpg", "https://cdntattoofilter.com/tattoo/377538/l.jpg", "https://cdntattoofilter.com/tattoo/329854/l.jpg"],
+        style: "Black-and-Grey",
+        description: "This style uses varying shades of black and grey ink to create realistic or stylized designs.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/235693/l.jpg", "https://cdntattoofilter.com/tattoo/155047/l.jpg", "https://cdntattoofilter.com/tattoo/4769/l.jpg"],
+        style: "New-School",
+        description: "A more cartoonish and exaggerated version of traditional tattooing, new school tattoos often feature bright colors, exaggerated proportions, and surreal or humorous designs.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/381688/l.jpg", "https://cdntattoofilter.com/tattoo/378739/l.jpg", "https://cdntattoofilter.com/tattoo/245189/l.jpg", "https://cdntattoofilter.com/tattoo/25943/l.jpg", "https://cdntattoofilter.com/tattoo/3991/l.jpg"],
+        style: "Realism",
+        description: "This style aims to create tattoos that look like photographs, often featuring portraits of people or animals.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/385795/l.jpg", "https://cdntattoofilter.com/tattoo/376037/l.jpg", "https://cdntattoofilter.com/tattoo/31115/l.jpg"],
+        style: "Neo-Traditional",
+        description: "A modern take on traditional tattooing, neo-traditional tattoos often incorporate more complex designs and a wider range of colors while still maintaining bold outlines.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/248651/l.jpg", "https://cdntattoofilter.com/tattoo/230184/l.jpg", "https://cdntattoofilter.com/tattoo/17457/l.jpg", "https://cdntattoofilter.com/tattoo/376626/l.jpg"],
+        style: "Biomechanical",
+        description: "This style features designs that look like machinery or other mechanical objects fused with human anatomy.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/21957/l.jpg", "https://cdntattoofilter.com/tattoo/31370/l.jpg", "https://cdntattoofilter.com/tattoo/260967/l.jpg"],
+        style: "Japanese/Irezumi",
+        description: "This style is inspired by traditional Japanese art and features large, detailed designs with vibrant colors and flowing lines. Common motifs include dragons, koi fish, and cherry blossoms.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/383906/l.jpg", "https://cdntattoofilter.com/tattoo/383903/l.jpg", "https://cdntattoofilter.com/tattoo/367587/l.jpg", "https://cdntattoofilter.com/tattoo/159119/l.jpg", "https://cdntattoofilter.com/tattoo/357122/l.jpg"],
+        style: "Blackwork",
+        description: "This style uses solid black ink to create intricate designs, often incorporating geometric shapes and patterns.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/348266/l.jpg", "https://cdntattoofilter.com/tattoo/348680/l.jpg", "https://cdntattoofilter.com/tattoo/327675/l.jpg", "https://cdntattoofilter.com/tattoo/330227/l.jpg"],
+        style: "Dotwork",
+        description: "Similar to blackwork, dotwork tattoos use tiny dots instead of solid lines to create complex designs.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/247288/l.jpg", "https://cdntattoofilter.com/tattoo/366106/l.jpg", "https://cdntattoofilter.com/tattoo/385104/l.jpg"],
+        style: "Traditional-Old-School",
+        description: "This style is characterized by bold, black outlines and a limited color palette of red, green, yellow, and blue. Common motifs include nautical symbols, animals, and pin-up girls.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/386300/l.jpg", "https://cdntattoofilter.com/tattoo/383486/l.jpg", "https://cdntattoofilter.com/tattoo/379921/l.jpg"],
+        style: "Watercolor",
+        description: "This style uses bright, bold colors and free-flowing brushstrokes to create designs that look like watercolor paintings.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/384476/l.jpg", "https://cdntattoofilter.com/tattoo/313455/l.jpg", "https://cdntattoofilter.com/tattoo/311428/l.jpg", "https://cdntattoofilter.com/tattoo/297724/l.jpg", "https://cdntattoofilter.com/tattoo/22223/l.jpg"],
+        style: "Tribal",
+        description: "This style is inspired by the traditional tattooing practices of indigenous peoples around the world and often features bold, black designs with geometric patterns.",
+    },
+    {
+        img: ["https://cdntattoofilter.com/tattoo/386746/l.jpg", "https://cdntattoofilter.com/tattoo/386804/l.jpg", "https://cdntattoofilter.com/tattoo/386467/l.jpg", "https://cdntattoofilter.com/tattoo/386078/l.jpg", "https://cdntattoofilter.com/tattoo/385241/l.jpg"],
+        style: "Lettering",
+        description: "This style includes any tattoo that features letters or words, often with elaborate calligraphy or other decorative elements.",
+    }
+]
