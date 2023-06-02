5115d4fed99c53356749456c8061b39f8c664234
Okeanid-ISR <78822546+Okeanid-ISR@users.noreply.github.com>
2023-06-02 16:25:57 +0300

    WIX: sendmail updated

--- functions/sendmail.js
+++ functions/sendmail.js
@@ -1,32 +1,44 @@
-    const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');
 
-    exports.handler = async function(event, context) {
-        if (event.httpMethod !== 'POST') {
-            return { statusCode: 405, body: 'Method Not Allowed' };
-        }
+exports.handler = async (event, context) => {
+    const { email, message } = JSON.parse(event.body);
 
-        const { email, message } = JSON.parse(event.body);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD,
+        },
+    });
 
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASSWORD,
+    const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: `New message from ${email}`,
+        text: message,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin" : "*", // Or the specific domain you want to allow
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET" // Specify the methods allowed
             },
-        });
-
-        const mailOptions = {
-            from: process.env.EMAIL,
-            to: process.env.EMAIL,
-            subject: `New message from ${email}`,
-            text: message,
+            body: JSON.stringify({ message: "Message sent successfully" })
         };
-
-        try {
-            await transporter.sendMail(mailOptions);
-            return { statusCode: 200, body: 'Message sent successfully' };
-        } catch (err) {
-            console.error(err);
-            return { statusCode: 500, body: 'Failed to send message' };
-        }
+    } catch (err) {
+        console.error(err);
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Origin" : "*", // Or the specific domain you want to allow
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET" // Specify the methods allowed
+            },
+            body: JSON.stringify({ message: 'Failed to send message' }),
+        };
     }
+}
